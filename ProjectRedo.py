4221c88a6a3269d06f76540e5381d6ee7fd67004
SamanthaBee-buzz <s.baker24@ncf.edu>
2025-12-19 12:59:14 -0500

    Update ProjectRedo.py

--- ProjectRedo.py
+++ ProjectRedo.py
@@ -5,18 +5,95 @@
 # Local URL: http://localhost:8501
 # Network URL: http://10.0.0.33:8501
 
-# Idea: top destinations for each country with tags
-
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# find country
+# tags
+# list destinations/activities
+# sort by popularity (# of ratings) or top rated (highest ratings)
+
 st.write('Select A Country:')
 CtryATrue = st.button('A')
 if CtryATrue:
     CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
 
+CtryBTrue = st.button('B')
+if CtryBTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
 
+CtryCTrue = st.button('C')
+if CtryCTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryDTrue = st.button('D')
+if CtryDTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryETrue = st.button('E')
+if CtryETrue:
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryFTrue = st.button('F')
+if CtryFTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryGTrue = st.button('G')
+if CtryGTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryHTrue = st.button('H')
+if CtryHTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryITrue = st.button('I')
+if CtryITrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryKTrue = st.button('K')
+if CtryKTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryLTrue = st.button('L')
+if CtryLTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryMTrue = st.button('M')
+if CtryMTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryNTrue = st.button('N')
+if CtryNTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryPTrue = st.button('P')
+if CtryPTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
+
+CtryRTrue = st.button('R')
+if CtryRTrue:
+    CtryAfghTrue = st.button('Afghanistan')
+    CtryAfghTrue = st.button('Afghanistan')
 # sources
-# https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations
+# https://simple.wikipedia.org/wiki/List_of_European_countries
