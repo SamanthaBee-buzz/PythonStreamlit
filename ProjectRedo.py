4eb9ebe3a60d4018f1ebd496c77fb97e42ab0ea8
SamanthaBee-buzz <s.baker24@ncf.edu>
2025-12-19 12:37:36 -0500

    Update ProjectRedo.py

--- ProjectRedo.py
+++ ProjectRedo.py
@@ -1,13 +1,22 @@
-import argparse
-import json
-import os
-from datetime import datetime, timezone
-from typing import Any, Dict, Iterable, List, Sequence
+# put the following into the terminal:
+# python -m pip install streamlit
+# python -m streamlit run .\ProjectRedo.py
 
-from dotenv import load_dotenv
-from newsapi import NewsApiClient
+# Local URL: http://localhost:8501
+# Network URL: http://10.0.0.33:8501
 
-from storage import insert_articles, log_ingestion
-from tavily_ingest import normalize_articles
+# Idea: top destinations for each country with tags
 
-top 5/10 destinations for each country?
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+st.write('Select A Country:')
+CtryATrue = st.button('A')
+if CtryATrue:
+    CtryAfghTrue = st.button('Afghanistan')
+
+
+# sources
+# https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations
