e93b42d9f77fee7cbc362c15b09fa5910dbc6f7c
SamanthaBee-buzz <s.baker24@ncf.edu>
2025-12-19 13:18:53 -0500

    Update ProjectRedo.py
    Edited the country selection interface

--- ProjectRedo.py
+++ ProjectRedo.py
@@ -1,6 +1,8 @@
 # put the following into the terminal:
 # python -m pip install streamlit
+# python -m pip install streamlit-option-menu
 # python -m streamlit run .\ProjectRedo.py
+
 
 # Local URL: http://localhost:8501
 # Network URL: http://10.0.0.33:8501
@@ -9,91 +11,19 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
 
 # find country
 # tags
 # list destinations/activities
 # sort by popularity (# of ratings) or top rated (highest ratings)
+st.title('Select A Country')
 
-st.write('Select A Country:')
-CtryATrue = st.button('A')
-if CtryATrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
+CtrySel = option_menu(None, ["A", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+if CtrySel == "A":
+    st.write("Albania")
+    st.write("Andorra")
+    st.write("Austria")
 
-CtryBTrue = st.button('B')
-if CtryBTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryCTrue = st.button('C')
-if CtryCTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryDTrue = st.button('D')
-if CtryDTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryETrue = st.button('E')
-if CtryETrue:
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryFTrue = st.button('F')
-if CtryFTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryGTrue = st.button('G')
-if CtryGTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryHTrue = st.button('H')
-if CtryHTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryITrue = st.button('I')
-if CtryITrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryKTrue = st.button('K')
-if CtryKTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryLTrue = st.button('L')
-if CtryLTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryMTrue = st.button('M')
-if CtryMTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryNTrue = st.button('N')
-if CtryNTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryPTrue = st.button('P')
-if CtryPTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-
-CtryRTrue = st.button('R')
-if CtryRTrue:
-    CtryAfghTrue = st.button('Afghanistan')
-    CtryAfghTrue = st.button('Afghanistan')
-# sources
-# https://simple.wikipedia.org/wiki/List_of_European_countries
