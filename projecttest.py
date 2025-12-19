72223e0485723cca182ef815bce19d3ddb633efa
SamanthaBee-buzz <s.baker24@ncf.edu>
2025-12-19 11:42:18 -0500

    Delete projecttest.py

--- projecttest.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import numpy as np
-import sys
-
-with st.chat_message("user"):
-    st.write("I am DataBot. What part of data analysis can I help you with?")
-    usertask = st.chat_input("...")
